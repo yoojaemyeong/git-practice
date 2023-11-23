2d3c847558d98a965cae990995743e91933526bb
jmyoo-one <yoojaemyeong+jmyoo-one@gmail.com>
2023-11-24 03:39:11 +0900

    [Div A] c func 복구

--- collaboration/c-func.c
+++ collaboration/c-func.c
@@ -5,3 +5,12 @@ int add(int a,int b){
 int sub(int a,int b){
 	return a-b;
 }
+int mul(int a,int b){
+	return a*b;
+}
+float divide(float a,float b){
+	if(b==0) 
+	    return -1;
+	else
+	    return a/b;
+}
